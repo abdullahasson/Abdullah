3a4583a8fc5952cb1f9ed854031a9e7ab0a6cdce
abdullahasson <bh532997@gmail.com>
2024-06-26 23:20:31 +0300

    remove images

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,14 +2,8 @@
 import MyPhoto from "./MyPhoto.png"
 // Profile Photo
 import HooBank from "./HooBank.png"
-// import hoobank_home from "./HooBank(2).jpg"
 import BrainWave from "./BrainWave (2).png"
-import BrainWave2 from "./BrainWave (1).png"
-// import Mount from "./Mount.jpg"
-import automato_home from "./Automato(1).jpg"
-import ISE from "./ISE(1).jpg"
-import todo from "./ToDo(1).jpg"
-import signIn from "./Sign-in(1).jpg"
+import ToDo from "./ToDo(1).jpg"
 
 
 export const HomePhoto = {
@@ -19,13 +13,6 @@ export const HomePhoto = {
 export const ProfilePhoto = {
     hoobank: HooBank,
     hoobankHash: "L66*%X4T?cM|?w9Ex]o#tQW?jEt7",
-    hoobank_home: hoobank_home,
-    mount: Mount,
     brainwave: BrainWave,
-    brainwave2: BrainWave2,
-    automato_home: automato_home,
-    ISE : ISE,
-    todo : todo,
-    signIn : signIn,
-
+    todo: ToDo
 }
